0591a361435fdfac376cd960dd98b722ce0f5c02
slamchillz <mendyslam@gmail.com>
2023-01-05 13:02:39 +0100

    basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Defines a function called 'concat'
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    Concatenates two strings
+    Args:
+        a (str)
+        b (str)
+    Return:
+        (str)
+    """
+    return a + b
