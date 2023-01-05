5c3858366454b7d0f209690241add4714abc1ad2
slamchillz <mendyslam@gmail.com>
2023-01-05 13:18:06 +0100

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+A module: Define and annotate variables of various types
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
