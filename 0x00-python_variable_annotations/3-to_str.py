a6d48890245f5385e55256054f5ce54c21b9ee9e
slamchillz <mendyslam@gmail.com>
2023-01-05 13:12:55 +0100

    basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Defines a function called 'to_str'
+"""
+
+
+def to_str(f: float) -> str:
+    """
+    Converts a float to string
+    Args:
+        f (float)
+    Return:
+        (str)
+    """
+    return str(f)
