db10f12dc95e5d399df93995e95d3c0a224779a3
slamchillz <mendyslam@gmail.com>
2023-01-10 12:38:54 +0100

    fix: chnaged return type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,12 +3,12 @@
 Defines an Asynchronous Comprehension
 """
 import asyncio
-from typing import Iterable
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Iterable[float]:
+async def async_comprehension() -> List[float]:
     """
     Collect 10 random numbers using an async comprehensing,
     then return the 10 random numbers.
