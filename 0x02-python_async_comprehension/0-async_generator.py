4b3af12d5f0fc377f8dac9020188dda73902d4ed
slamchillz <mendyslam@gmail.com>
2023-01-10 12:32:03 +0100

    fix: uniform to random

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,14 +4,14 @@ Defines an Async Generator
 """
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Loop 10 times, each time asynchronously
     wait 1 second, then yield a random number between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
