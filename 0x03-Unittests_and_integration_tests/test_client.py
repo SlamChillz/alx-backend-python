96ea09b51bf4352da21fa54510deca87bcc6e130
slamchillz <mendyslam@gmail.com>
2023-01-30 08:55:13 +0100

    test for _public_repo_url property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,6 +5,7 @@ Tests for GithubOrgClient class methods
 import unittest
 from unittest.mock import patch
 from unittest.mock import MagicMock
+from unittest.mock import PropertyMock
 from parameterized import parameterized
 
 import client
@@ -29,3 +30,16 @@ class TestGithubOrgClient(unittest.TestCase):
         gitClient = GithubOrgClient(org)
         self.assertEqual(gitClient.org, getJson.return_value)
         getJson.assert_called_once_with(gitClient.ORG_URL.format(org=org))
+
+    def test_public_repos_url(self):
+        """
+        Test _public_repos_url property
+        """
+        config = {'return_value':
+                  {'repos_url': 'https://api.github.com/slam/repos/alx'}}
+
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock, **config) as mockOrg:
+            test = GithubOrgClient('alx')
+            self.assertEqual(test._public_repos_url,
+                             mockOrg.return_value['repos_url'])
