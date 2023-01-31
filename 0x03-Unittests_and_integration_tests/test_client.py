2cbd5bd2f9faa98b532b1aefddec98ae5697451d
slamchillz <mendyslam@gmail.com>
2023-01-31 01:28:10 +0100

    rearranged class decorator

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -97,9 +97,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
 
 @parameterized_class(('org_payload', 'repos_payload',
-                      'expected_repos', 'apache2_repos'), [
-                      payloads for payloads in TEST_PAYLOAD
-])
+                      'expected_repos', 'apache2_repos'), TEST_PAYLOAD)
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """
     Integration test for GithubOrgClient
